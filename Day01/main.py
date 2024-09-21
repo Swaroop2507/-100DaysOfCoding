5817e37cab590130d3c64d86609eaa43e8eabe71
Swaroop Sakharkar <67748579+Swaroop2507@users.noreply.github.com>
2024-09-21 17:59:36 +0530

    Create main.py
    day01---marked with success

---
+++ Day01/main.py
@@ -0,0 +1,6 @@
+#Create a greeting for your program.
+print("Hola, users!")
+# Ask the user for the city that they grew up in and store it in a variable.
+cityname = input("What is your city name?\n")
+petname = input("what is your pet name?\n")
+print(cityname+" "+petname)
